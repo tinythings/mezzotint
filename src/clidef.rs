dc78bb0d22e654aa6cfac9ce1854042132a7dd01
Bo Maryniuk <bo@maryniuk.net>
2023-09-07 18:07:44 +0200

    Main app scaffold

---
+++ src/clidef.rs
@@ -0,0 +1,49 @@
+use clap::builder::styling;
+use clap::{Arg, ArgAction, Command};
+use colored::Colorize;
+
+/// Define CLI arguments and styling
+pub fn cli(version: &'static str) -> Command {
+    let styles = styling::Styles::styled()
+        .header(styling::AnsiColor::White.on_default() | styling::Effects::BOLD)
+        .usage(styling::AnsiColor::White.on_default() | styling::Effects::BOLD)
+        .literal(styling::AnsiColor::BrightCyan.on_default())
+        .placeholder(styling::AnsiColor::Cyan.on_default());
+
+    Command::new("mezzotint")
+        .version(version)
+        .about(format!("{}{} - {}", "mezzo".bold().underline(), "tint", "is a tool to turn your container into an App Bundle"))
+        // Config
+        .arg(
+            Arg::new("exe")
+                .short('x')
+                .long("exe")
+                .help("Specify path to an executable which needs to be preserved.")
+                .value_delimiter(','),
+        )
+        .arg(
+            Arg::new("profile")
+                .short('p')
+                .long("profile")
+                .action(ArgAction::SetTrue)
+                .help("Profile, describing whole setup"),
+        )
+        // Other
+        .arg(
+            Arg::new("debug")
+                .short('d')
+                .long("debug")
+                .action(ArgAction::SetTrue)
+                .help("Set debug mode for more verbose output."),
+        )
+        .arg(
+            Arg::new("version")
+                .short('v')
+                .long("version")
+                .action(ArgAction::SetTrue)
+                .help("Get current version."),
+        )
+        .disable_version_flag(true)
+        .disable_colored_help(false)
+        .styles(styles)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+mod clidef;
+mod logger;
+
+static VERSION: &str = "0.1";
+static LOGGER: logger::STDOUTLogger = logger::STDOUTLogger;
+
+fn main() -> Result<(), std::io::Error> {
+    let args: Vec<String> = env::args().collect();
+    let mut cli = clidef::cli(VERSION);
+
+    if args.len() == 1 {
+        return {
+            cli.print_help().unwrap();
+            Ok(())
+        };
+    }
+
+    let params = cli.to_owned().get_matches();
+
+    // Setup logger
+    if let Err(err) = log::set_logger(&LOGGER)
+        .map(|()| log::set_max_level(if params.get_flag("debug") { log::LevelFilter::Trace } else { log::LevelFilter::Info }))
+    {
+        return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, err.to_string()));
+    }
+
+    Ok(())
 }
