9455e7899a1c47e0babb9ccfd4a091518f237b04
Bo Maryniuk <bo@maryniuk.net>
2023-11-02 14:07:00 +0100

    Update extensions and replace hardcoded values

--- src/filters/defs.rs
+++ src/filters/defs.rs
@@ -7,10 +7,13 @@ pub const DOC_STUB_FILES: &[&str] =
     &["AUTHORS", "COPYING", "LICENSE", "DEBUG", "DISTRIB", "DOC", "HISTORY", "README", "TERMS", "TODO"];
 
 /// Docfiles
-pub const DOC_F_EXT: &[&str] = &[".txt", ".doc", ".rtf", ".md", ".rtx", ".tex", ".xml"];
+pub const DOC_F_EXT: &[&str] = &[".txt", ".doc", ".rtf", ".md", ".rtx", ".tex", ".xml", ".htm", ".html", ".log"];
 
 /// Docfiles portable
 pub const DOC_FP_EXT: &[&str] = &[".eps", ".pdf", ".ps"];
+
+/// Typically, docs
+pub const DOC_LOCATIONS: &[&str] = &["/usr/share/doc"];
 
 /// Headers
 pub const H_SRC_F_EXT: &[&str] = &[".h", ".hpp"];
@@ -19,5 +22,16 @@ pub const H_SRC_F_EXT: &[&str] = &[".h", ".hpp"];
 pub const ARC_F_EXT: &[&str] = &[".gz", ".bz2", ".xz", ".zip", ".tar"];
 
 /// Graphic files
-pub const IMG_F_EXT: &[&str] =
-    &[".bmp", ".jpg", ".jpeg", ".png", ".gif", ".xpm", ".xbm", ".tif", ".tiff", ".pbm", ".svg", ".ico"];
+pub const IMG_F_EXT: &[&str] = &[
+    ".ani", ".bmp", ".dib", ".pcx", ".jpg", ".jpeg", ".jpx", ".jxr", ".png", ".gif", ".xpm", ".xbm", ".tif", ".tiff", ".iff",
+    ".lbm", ".pbm", ".pgm", ".pict", ".svg", ".ico", ".ai",
+];
+
+/// Manpages
+pub const D_MANPAGES: &str = "/usr/share/man";
+
+/// Localisation
+pub const D_L10N: &str = "/usr/share/locale";
+
+/// Intetrnaetiomns... i18n
+pub const D_I18N: &str = "/usr/share/i18n";
